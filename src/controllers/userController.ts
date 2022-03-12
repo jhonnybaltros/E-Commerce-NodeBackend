4bc8066a0952c5ae13230de7b55263264764e21b
JhonnyBaltros <jhonnybaixo@gmail.com>
2022-03-12 17:51:35 -0300

    Eslint improvement

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,12 +1,12 @@
-import { Request, Response } from "express";
-import generateToken from "../utils/generateToken";
-import asyncHandler from "express-async-handler";
-import User from "../models/userModels";
+import { Request, Response } from 'express';
+import generateToken from '../utils/generateToken';
+import asyncHandler from 'express-async-handler';
+import User from '../models/userModels';
 
 const authUser = asyncHandler(async (req: Request, res: Response) => {
   const { email, password } = req.body;
 
-  const user =  await User.findOne({ email })
+  const user = await User.findOne({ email });
 
   if (user && (await user.matchPassword(password))) {
     res.json({
@@ -14,29 +14,29 @@ const authUser = asyncHandler(async (req: Request, res: Response) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdm,
-      token: generateToken(user._id)
-    })
-  }else {
-    res.status(401)
-      throw new Error('Invalid email, or password')
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid email, or password');
   }
 });
 
 const registerUser = asyncHandler(async (req: Request, res: Response) => {
   const { name, email, password } = req.body;
 
-  const userExists =  await User.findOne({ email })
+  const userExists = await User.findOne({ email });
 
   if (userExists) {
-    res.status(400)
-    throw new Error('User Already exists')
+    res.status(400);
+    throw new Error('User Already exists');
   }
 
   const user = await User.create({
     name,
     email,
-    password
-  })
+    password,
+  });
 
   if (user) {
     res.status(201).json({
@@ -44,28 +44,28 @@ const registerUser = asyncHandler(async (req: Request, res: Response) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdm,
-      token: generateToken(user._id)
-    })
+      token: generateToken(user._id),
+    });
   } else {
-    res.status(400)
-    throw new Error('Invalid user data')
+    res.status(400);
+    throw new Error('Invalid user data');
   }
 });
 
 const getUserProfile = asyncHandler(async (req: any, res: Response) => {
-    const user = await User.findById(req.user._id)
+  const user = await User.findById(req.user._id);
 
-    if (user) {
-      res.json({
-       _id: user._id,
+  if (user) {
+    res.json({
+      _id: user._id,
       name: user.name,
       email: user.email,
       isAdmin: user.isAdm,
-      })
-    } else {
-      res.status(404)
-      throw new Error('User not found')
-    }
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
 });
 
 export { authUser, getUserProfile, registerUser };
