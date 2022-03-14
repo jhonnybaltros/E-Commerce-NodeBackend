e34bde479eb6e2ddae4aebcf25d551f51dc17749
JhonnyBaltros <jhonnybaixo@gmail.com>
2022-03-13 22:24:05 -0300

    Fix in productRoutes.ts

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -1,15 +1,15 @@
-import { Request, Response } from "express";
-import asyncHandler from "express-async-handler";
-import Product from "../models/productModel";
+import { Request, Response } from 'express';
+import asyncHandler from 'express-async-handler';
+import Product from '../models/productModel';
 
 // @desc Fetch all products
 // @route GET /api/products
 // @access Public
 
 const getProducts = asyncHandler(async (req: Request, res: Response) => {
-  const products = await Product.find({});
+	const products = await Product.find({});
 
-  res.json(products);
+	res.json(products);
 });
 
 // @desc Fetch a single product
@@ -17,16 +17,14 @@ const getProducts = asyncHandler(async (req: Request, res: Response) => {
 // @access Public
 
 const getProductsById = asyncHandler(async (req: Request, res: Response) => {
-  asyncHandler(async (req: Request, res: Response) => {
-    const product = await Product.findById(req.params.id);
+	const product = await Product.findById(req.params.id);
 
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error(`Product not found`);
-    }
-  }); 
+	if (product) {
+		res.json(product);
+	} else {
+		res.status(404);
+		throw new Error(`Product not found`);
+	}
 });
 
 export { getProducts, getProductsById };
